70981b30c2c3a9906931640dc12ec1d047d4334b
SamSepiolProxy <57568210+SamSepiolProxy@users.noreply.github.com>
2025-02-12 11:18:15 +0000

    Update Get-DefaultDomainPasswordPolicy.ps1

--- Get-DefaultDomainPasswordPolicy.ps1
+++ Get-DefaultDomainPasswordPolicy.ps1
@@ -1,4 +1,4 @@
-# Function to get the default domain password policy including reversible encryption
+# Function to get the default domain password policy including reversible encryption and observation window
 function Get-DefaultPasswordPolicy {
     try {
         # Get the domain context
@@ -8,9 +8,17 @@ function Get-DefaultPasswordPolicy {
         $searcher.SearchRoot = [ADSI]("LDAP://" + $defaultNamingContext)
         $searcher.Filter = "(objectClass=domain)"
 
-        # Add properties to retrieve
-        $properties = @("minPwdLength", "maxPwdAge", "pwdHistoryLength", "pwdProperties", 
-                        "lockoutDuration", "lockoutThreshold", "ms-DS-Password-Reversible-Encryption-Enabled")
+        # Add properties to retrieve (including lockoutObservationWindow)
+        $properties = @(
+            "minPwdLength", 
+            "maxPwdAge", 
+            "pwdHistoryLength", 
+            "pwdProperties", 
+            "lockoutDuration", 
+            "lockoutThreshold", 
+            "ms-DS-Password-Reversible-Encryption-Enabled",
+            "lockoutObservationWindow"
+        )
         foreach ($prop in $properties) {
             $searcher.PropertiesToLoad.Add($prop) | Out-Null
         }
@@ -29,13 +37,17 @@ function Get-DefaultPasswordPolicy {
             $lockoutDuration = if ($domain["lockoutDuration"].Count -gt 0) { [timespan]::FromTicks([int64]$domain["lockoutDuration"][0]) } else { $null }
             $lockoutDurationMinutes = if ($lockoutDuration -ne $null) { -$lockoutDuration.TotalMinutes } else { "Not Set" }
 
+            # Convert lockoutObservationWindow to minutes
+            $lockoutObservationWindow = if ($domain["lockoutObservationWindow"].Count -gt 0) { [timespan]::FromTicks([int64]$domain["lockoutObservationWindow"][0]) } else { $null }
+            $lockoutObservationWindowMinutes = if ($lockoutObservationWindow -ne $null) { -$lockoutObservationWindow.TotalMinutes } else { "Not Set" }
+
             # Get password properties
             $pwdProperties = if ($domain["pwdProperties"].Count -gt 0) { $domain["pwdProperties"][0] -as [int] } else { 0 }
 
             # Password Complexity (Bit 0 - Value 1)
             $passwordComplexityEnabled = if (($pwdProperties -band 1) -eq 1) { "Enabled" } else { "Disabled" }
 
-            # Store Passwords Using Reversible Encryption (ms-DS-Password-Reversible-Encryption-Enabled)
+            # Store Passwords Using Reversible Encryption
             $reversibleEncryption = if ($domain["ms-DS-Password-Reversible-Encryption-Enabled"].Count -gt 0) {
                 if ($domain["ms-DS-Password-Reversible-Encryption-Enabled"][0] -eq "TRUE") { "Enabled" } else { "Disabled" }
             } else {
@@ -51,6 +63,7 @@ function Get-DefaultPasswordPolicy {
             Write-Host "Password Complexity: " $passwordComplexityEnabled
             Write-Host "Store Passwords Using Reversible Encryption: " $reversibleEncryption
             Write-Host "Account Lockout Duration (Minutes): " $lockoutDurationMinutes
+            Write-Host "Account Lockout Observation Window (Minutes): " $lockoutObservationWindowMinutes
             Write-Host "Account Lockout Threshold: " ($domain["lockoutThreshold"][0] -as [int])
         } else {
             Write-Host "Could not retrieve password policy."
