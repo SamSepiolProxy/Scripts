4176f070ceb9e589126a79a5d27963d7a95f54e4
SamSepiolProxy <57568210+SamSepiolProxy@users.noreply.github.com>
2025-03-20 09:24:17 +0000

    Update ip-range.py

--- Other/ip-range.py
+++ Other/ip-range.py
@@ -1,19 +1,38 @@
-#!/usr/bin/python
-#example python3 ip-range.py 192.168.0.0 192.168.0.255
+#!/usr/bin/python3
 import sys
+import ipaddress
 
-def undotIPv4 (dotted):
-    return sum (int (octet) << ( (3 - i) << 3) for i, octet in enumerate (dotted.split ('.') ) )
+def undotIPv4(dotted):
+    return sum(int(octet) << ((3 - i) << 3) for i, octet in enumerate(dotted.split('.')))
 
-def dotIPv4 (addr):
-    return '.'.join (str (addr >> off & 0xff) for off in (24, 16, 8, 0) )
+def dotIPv4(addr):
+    return '.'.join(str(addr >> off & 0xff) for off in (24, 16, 8, 0))
 
-def rangeIPv4 (start, stop):
-    for addr in range (undotIPv4 (start), undotIPv4 (stop) ):
-        yield dotIPv4 (addr)
+def rangeIPv4(start, stop):
+    # Generate IPs from start (inclusive) to stop (exclusive)
+    for addr in range(undotIPv4(start), undotIPv4(stop)):
+        yield dotIPv4(addr)
 
-for x in rangeIPv4 (sys.argv[1], sys.argv[2]):
-    print(x)
+def print_usage():
+    print("Usage:")
+    print("  ip-range.py start_ip end_ip")
+    print("  ip-range.py network/mask")
+    sys.exit(1)
 
-print(sys.argv[2])
-
+if len(sys.argv) == 2:
+    # Assume CIDR notation was provided
+    try:
+        network = ipaddress.ip_network(sys.argv[1], strict=False)
+        for ip in network:
+            print(ip)
+    except ValueError as e:
+        print(f"Invalid network: {e}")
+        sys.exit(1)
+elif len(sys.argv) == 3:
+    # Assume start and stop IP addresses were provided
+    for x in rangeIPv4(sys.argv[1], sys.argv[2]):
+        print(x)
+    # The original script printed the final IP separately
+    print(sys.argv[2])
+else:
+    print_usage()
